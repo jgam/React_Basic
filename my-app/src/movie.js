99d7c253819c21a014cb625b51ea3103994aed95
jgam <jgam@alumni.nd.edu>
2019-04-23 16:29:31 +0900

    this is functional(dumb) component and it is simple

--- my-app/src/movie.js
+++ my-app/src/movie.js
@@ -2,29 +2,22 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';//this checks the type of prop
 import './movie.css';
 
-class Movie extends Component {
 
-    static propTypes = {//this checks which kind of datatypes and if it is required or not
-        title: PropTypes.string,
-        poster: PropTypes.string
-    }
 
-    render(){
-        return(
-            <div>
-                <MoviePoster poster={this.props.poster}/>
-                <h1>{this.props.title}</h1>
-            </div>
-        )
-    }
+function Movie({title, poster}){
+    return(
+        <div>
+            <MoviePoster poster={poster} />
+            <h1>{title}</h1>
+        </div>
+    )
 }
 
-class MoviePoster extends Component{
-    render(){
-        return(
-            <img src={this.props.poster}/>
-        )
-    }
+function MoviePoster({poster}){
+    return(
+        <img src={poster} alt="Movie Poster" />
+    )
 }
+
 
 export default Movie;
