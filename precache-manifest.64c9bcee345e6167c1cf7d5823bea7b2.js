3db3cdd849178ebacd96659fa785ad15304f5459
jgam <jgam@alumni.nd.edu>
2019-04-24 17:57:38 +0900

    Updates

---
+++ precache-manifest.64c9bcee345e6167c1cf7d5823bea7b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75669528e622696b7118",
+    "url": "/korin_i/projects/movie_app/static/css/main.bf8b11e1.chunk.css"
+  },
+  {
+    "revision": "75669528e622696b7118",
+    "url": "/korin_i/projects/movie_app/static/js/main.4d6b7b8f.chunk.js"
+  },
+  {
+    "revision": "36e4eebf88cea5442c19",
+    "url": "/korin_i/projects/movie_app/static/js/runtime~main.531b8138.js"
+  },
+  {
+    "revision": "6303b1dc6d95fb6846a7",
+    "url": "/korin_i/projects/movie_app/static/js/2.9dee5afb.chunk.js"
+  },
+  {
+    "revision": "2e55ed20b06e949ccf9c4afaebaffe1d",
+    "url": "/korin_i/projects/movie_app/index.html"
+  }
+];
