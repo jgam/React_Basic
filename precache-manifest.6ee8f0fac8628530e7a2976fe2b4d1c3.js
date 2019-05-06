793920062db66c284c11224f8d82ce9c2d1b87fa
jgam <jgam@alumni.nd.edu>
2019-05-06 21:46:16 +0900

    Updates

---
+++ precache-manifest.6ee8f0fac8628530e7a2976fe2b4d1c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8567997d219191d6bdae",
+    "url": "/React_Basic/static/js/runtime~main.458c2b7c.js"
+  },
+  {
+    "revision": "d5441531a0e74399bc5c",
+    "url": "/React_Basic/static/js/main.bb2c0bb7.chunk.js"
+  },
+  {
+    "revision": "2c6e31727b5458af87d5",
+    "url": "/React_Basic/static/js/2.c44e2cb5.chunk.js"
+  },
+  {
+    "revision": "d5441531a0e74399bc5c",
+    "url": "/React_Basic/static/css/main.0bdc8b33.chunk.css"
+  },
+  {
+    "revision": "f421992296f2bfb374eae2b1a9c6a83a",
+    "url": "/React_Basic/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/korin_i/React_Basic/precache-manifest.b44277ae448e6b492defb5bc0d1d809a.js"
+  "/React_Basic/precache-manifest.6ee8f0fac8628530e7a2976fe2b4d1c3.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/korin_i/React_Basic/index.html", {
+workbox.routing.registerNavigationRoute("/React_Basic/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
